ddff6b9cc3f8417761e4e9cd9b5b21574ba4a535
Jesse Gerard Brands <jessebrands@icloud.com>
2022-08-05 13:10:21 +0200

    include Spring Security OAuth2 Authorization Server

--- src/main/java/com/zeldaspeedruns/zeldaspeedruns/security/SecurityConfig.java
+++ src/main/java/com/zeldaspeedruns/zeldaspeedruns/security/SecurityConfig.java
@@ -1,12 +1,36 @@
 package com.zeldaspeedruns.zeldaspeedruns.security;
 
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OidcConfigurer;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.ProviderSettings;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
 
 @Configuration
 @EnableWebSecurity
@@ -17,6 +41,20 @@ public class SecurityConfig {
     }
 
     @Bean
+    @Order(1)
+    public SecurityFilterChain authorizationServerFilterChain(HttpSecurity httpSecurity) throws Exception {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(httpSecurity);
+
+        httpSecurity
+                .exceptionHandling((exceptions) -> {
+                    exceptions.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/user/login"));
+                });
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    @Order(2)
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .authorizeRequests(requests -> {
@@ -42,5 +80,60 @@ public class SecurityConfig {
                 });
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    public RegisteredClientRepository registeredClientRepository(JdbcOperations jdbcOperations) {
+        var repository = new JdbcRegisteredClientRepository(jdbcOperations);
+        var client = repository.findByClientId("zeldaspeedruns");
+
+        if (client == null) {
+            client = RegisteredClient.withId(UUID.randomUUID().toString())
+                    .clientId("zeldaspeedruns")
+                    .clientSecret("{bcrypt}" + passwordEncoder().encode("secret"))
+                    .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                    .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                    .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                    .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                    .redirectUri("http://127.0.0.1:8080/user/login/oauth2/code/messaging-client-oidc")
+                    .scope(OidcScopes.OPENID)
+                    .clientSettings(ClientSettings.builder().requireAuthorizationConsent(false).build())
+                    .build();
+
+            repository.save(client);
+        }
+
+        return repository;
+    }
+
+    @Bean
+    public JWKSource<SecurityContext> jwkSource() {
+        KeyPair keyPair = generateRsaKey();
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+        RSAKey rsaKey = new RSAKey.Builder(publicKey)
+                .privateKey(privateKey)
+                .keyID(UUID.randomUUID().toString())
+                .build();
+        JWKSet jwkSet = new JWKSet(rsaKey);
+        return new ImmutableJWKSet<>(jwkSet);
+    }
+
+    private static KeyPair generateRsaKey() {
+        KeyPair keyPair;
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            keyPair = keyPairGenerator.generateKeyPair();
+        }
+        catch (Exception ex) {
+            throw new IllegalStateException(ex);
+        }
+        return keyPair;
+    }
+
+    @Bean
+    public ProviderSettings providerSettings() {
+        return ProviderSettings.builder().build();
     }
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -151,3 +151,22 @@ create table if not exists organization_invite_uses
             on update cascade
 );
 
+
+---
+--- Spring Security OAuth2 Authorization Server
+---
+create table if not exists oauth2_registered_client
+(
+    id                            varchar(100) primary key not null,
+    client_id                     varchar(100)             not null,
+    client_id_issued_at           timestamp                not null default now(),
+    client_secret                 varchar(200)                      default null,
+    client_secret_expires_at      timestamp                         default null,
+    client_name                   varchar(200)             not null,
+    client_authentication_methods varchar(1000)            not null,
+    authorization_grant_types     varchar(1000)            not null,
+    redirect_uris                 varchar(1000)                     default null,
+    scopes                        varchar(1000)            not null,
+    client_settings               varchar(2000)            not null,
+    token_settings                varchar(2000)            not null
+);
